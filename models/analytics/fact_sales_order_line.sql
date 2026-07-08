6f8d4c5afed15e7c563d4c99e693316e3742c2db
hoanggiang <bongxynh.ly@gmail.com>
2026-07-08 16:17:32 +0700

    add gross amount

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -1,4 +1,5 @@
 -- test
 SELECT 
-   order_line_id as sales_order_line_key, quantity as quantity, unit_price as unit_price
+   order_line_id as sales_order_line_key, quantity , unit_price ,
+   unit_price*quantity as goross_amount
 FROM `vit-lam-data.wide_world_importers.sales__order_lines`  
